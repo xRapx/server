1cd9bb34d927d6118557fabc8fb08c8de1d58323
xRapx <102133684+xRapx@users.noreply.github.com>
2024-03-18 18:07:15 +0700

    #7 Custom Dynamic Middleware for ApiKey and Permissions

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const {findById} = require('../services/apikey.service')
+
+const HEADER = {
+	API_KEY:'x-api-key',
+	AUTHORIZATION : 'authorization'
+}
+
+//Middleware
+const apiKey = async (req, res, next) => {
+	try {
+		const key = req.headers[HEADER.API_KEY]?.toString()
+		if(!key){
+			return res.status(403).json({
+				message: "Forbidden Error 1"
+			})
+		}
+
+		//check objectKey
+		const objKey = await findById(key)
+		if(!objKey) {
+			return res.status(403).json({
+                message: "Forbidden Error 2"
+            })
+		}
+		// Nếu tồn tại
+		req.objKey = objKey;
+		
+		return next();
+
+	} catch (error) {
+		
+	}
+}
+//check permisstions lưu ý số ít số nhiều
+const permission = (permission) =>{
+	return (req,res,next) =>{
+		if(!req.objKey.permissions){
+			return res.status(403).json({
+                message: "Permission Error Denied 1"
+            })
+		}
+		
+		console.log(`permissions :: `,req.objKey.permissions)
+		const validPermission = req.objKey.permissions.includes(permission)
+		if(!validPermission){
+			return res.status(403).json({
+                message: "Permission Error Denied 2"
+            })
+		}
+		return next();
+	}
+}
+
+module.exports = {
+    apiKey,
+	permission
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
 'use strict'
 
 const express = require('express')
+const { apiKey,permission } = require('../auth/checkAuth')
 const router = express.Router()
 
+//check apiKey middleware
+router.use(apiKey)
+
+//check permission
+router.use(permission('0000'))
+
 router.use('/v1/api', require('./access'))
-// router.get('', (req, res, next) =>{
-// 	return res.status(200).json({
-//         message: "Hello World"
-//     })
-// })
 
 module.exports = router
