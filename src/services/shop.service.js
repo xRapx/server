a1e2f8023be9ef9bdfce1e8a6d3812763190cd1b
xRapx <102133684+xRapx@users.noreply.github.com>
2024-04-03 10:18:39 +0700

    fix bug #3

--- src/services/shop.service.js
+++ src/services/shop.service.js
@@ -2,7 +2,7 @@
 const shopModel = require('../models/shop.model');
 
 const findByEmail = async ({email , select = {
-	email : 1 , password : 2 , name : 1, status : 1, roles : 1, pic: 1 // Add 'pic' field to the select object
+	email : 1 , password : 2 , name : 1, status : 1, roles : 1, avatar: 1 // Add 'pic' field to the select object
 }}) => {
 	return await shopModel.findOne({email}).select(select).lean();
 };
